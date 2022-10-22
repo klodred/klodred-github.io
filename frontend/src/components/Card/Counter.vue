1156a246c38429fae9d253f4021b6af97129f977
klodred <aliceinzazerkalie@gmail.com>
2022-10-22 12:44:56 +0300

    перенос на vue.js

---
+++ frontend/src/components/Card/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-flex">
+
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-sm-4 col-xs-4 col-md-5 col-lg-5">
+          <div class="input-group mb-4">
+            <div class="input-group-prepend">
+              <button class="btn btn-dark btn-sm" id="minus-btn"><i
+                  class="fa fa-minus"></i></button>
+            </div>
+            <input style="text-align: center" type="number" id="qty_input"
+                   class="form-control form-control-sm" value="1" min="1">
+            <div class="input-group-prepend">
+              <button class="btn btn-dark btn-sm" id="plus-btn"><i
+                  class="fa fa-plus"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Main/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+
+    <TopNavbar></TopNavbar>
+    <CatalogNavbar></CatalogNavbar>
+
+    <!--
+    <nav class="nav navbar-expand-lg py-2 navbar-dark bg-dark text-bg-light">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+
+    </nav>
+    -->
+  </header>
+</template>
+
+<script>
+import DropdownMenu from "@/components/Menu/DropdownMenu";
+import CatalogNavbar from "@/components/Navbar/CatalogNavbar";
+import TopNavbar from "@/components/Navbar/TopNavbar";
+export default {
+  name: "Header",
+  components: {TopNavbar, CatalogNavbar, DropdownMenu}
+}
+</script>
+
+<style scoped>
+
+
+</style>
